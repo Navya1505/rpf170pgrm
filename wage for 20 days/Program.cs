08f9d5156e79dd6a581fb3bd52f90fa573e4389d
Navya1505 <Navya1505@gmail.com>
2022-07-12 15:08:48 +0530

    wage for 20 days prgrm is added

---
+++ wage for 20 days/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace wage_for_20_days
+{
+    public static class Wage_for_20_days
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
+        public static void Main(string[] args)
+        { }
+
+        public static void wage_for_20_days()
+        {
+            {
+
+
+                int emphrs = 0;
+                int empwage = 0;
+                int totalempwage = 0;
+                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+                {
+                    Random random = new Random();
+                    int empcheck = random.Next(0, 3);
+                    switch (empcheck)
+                    {
+                        case IS_PART_TIME:
+                            emphrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            emphrs = 8;
+                            break;
+                        default:
+                            emphrs = 0;
+                            break;
+                    }
+                    empwage = emphrs * EMP_RATE_PER_HOUR;
+                    totalempwage += empwage;
+                    Console.WriteLine("empwage:" + empwage);
+
+                }
+                Console.WriteLine("Total empwage:" + totalempwage);
+            }
+        }
+    }
+}
